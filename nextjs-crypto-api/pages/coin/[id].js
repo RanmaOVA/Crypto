5ea3d8309b2630d3a18443f5c63bed26d92747a6
RanmaOVA <Masseymichael643@gmail.com>
2022-02-02 22:45:01 -0600

    editting id file

---
+++ nextjs-crypto-api/pages/coin/[id].js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout'
+
+const Coin = ({ coin }) => {
+    return (
+        <Layout>
+            <div className={}>
+
+            </div>
+        </Layout>
+    )
+}
